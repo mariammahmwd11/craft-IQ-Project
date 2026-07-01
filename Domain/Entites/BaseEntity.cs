3416327b0304c0e35fd84dae499dfa94259e8955
Hardware <Hardware@DESKTOP-JR191S6>
2026-07-01 16:48:06 +0300

    add models

---
+++ Domain/Entites/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entites
+{
+    public class BaseEntity
+    {
+        public int ID { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public Guid ModifiedBy { get; set; }
+        public DateTimeOffset ModifiedOn { get; set; }
+    }
+}
--- Infrastructure/Presistance/Data/AppDbContext.cs
+++ Infrastructure/Presistance/Data/AppDbContext.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CraftIQ.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace Infrastructure.Presistance.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Product> products => Set<Product>();
+        public DbSet<Inventory> inventories => Set<Inventory>();
+        public DbSet<Order> orders => Set<Order>();
+        public DbSet<OrderDetail> OrderDetails => Set<OrderDetail>();
+        public DbSet<Transaction> transactions => Set<Transaction>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+        }
     }
 }
